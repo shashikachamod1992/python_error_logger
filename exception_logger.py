60d3b515e9dc321037d27ba5f8157beed67a9ba7
Shashika1992 <shashikachamod4u@gmail.com>
2023-02-28 00:14:22 +0530

    fix bug log file name

--- exception_logger.py
+++ exception_logger.py
@@ -8,7 +8,7 @@ logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
 def log_exceptions(exception_traceback):
     now = datetime.now()
-    message = '\n' + '\n' + 'Log no - ' + log_no + '\n' + now.strftime(
+    message = '\n' + '\n'+ '\n' + now.strftime(
         "%m/%d/%Y, %H:%M:%S") +'\n'+ exception_traceback + '\n'
     logging.exception(message)
 
