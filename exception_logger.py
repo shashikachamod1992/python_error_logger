89d329a6fe5e3c27e666616cb95894897f09e7f3
Shashika1992 <shashikachamod4u@gmail.com>
2023-02-28 00:08:48 +0530

    add a main program

--- exception_logger.py
+++ exception_logger.py
@@ -5,8 +5,17 @@ LOG_FILENAME = 'error_log.out'
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
 
-def log_exceptions(log_no, exception_traceback):
+def log_exceptions(exception_traceback):
     now = datetime.now()
     message = '\n' + '\n' + 'Log no - ' + log_no + '\n' + now.strftime(
         "%m/%d/%Y, %H:%M:%S") +'\n'+ exception_traceback + '\n'
     logging.exception(message)
+
+
+if __name__ == '__main__':
+    try:
+        x = 5 / 0
+    except:
+        log_exceptions(str(traceback.format_stack()))
+
+
