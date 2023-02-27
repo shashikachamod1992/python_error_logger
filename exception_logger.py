64e55376330c1987244fffd5137e11cc4ee10d68
Shashika1992 <shashikachamod4u@gmail.com>
2023-02-28 00:20:12 +0530

    add useful comments to main file

--- exception_logger.py
+++ exception_logger.py
@@ -1,3 +1,5 @@
+
+# imports
 import logging
 from datetime import datetime
 import traceback
@@ -9,14 +11,14 @@ logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 def log_exceptions(exception_traceback):
     now = datetime.now()
     message = '\n' + '\n'+ '\n' + now.strftime(
-        "%m/%d/%Y, %H:%M:%S") +'\n'+ exception_traceback + '\n'
-    logging.exception(message)
+        "%m/%d/%Y, %H:%M:%S") +'\n'+ exception_traceback + '\n' # this is the custom message to be logged. you can change this your own way.
+    logging.exception(message) #logging will append the customized error message to the end of the document
 
 
 if __name__ == '__main__':
     try:
         x = 5 / 0
     except:
-        log_exceptions(str(traceback.format_stack()))
+        log_exceptions(str(traceback.format_stack())) # traceback is used to print the complete error stack. this is convenient for debugging
 
 
